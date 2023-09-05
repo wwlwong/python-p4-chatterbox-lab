fb74d9fc9d98f4a8acf804561924f0e0e7b130a1
William Wong <william.wai.lim.wong@gmail.com>
2023-09-04 20:49:50 -0400

    passes all tests

--- server/app.py
+++ server/app.py
@@ -14,13 +14,90 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods = ['GET', 'POST'])
 def messages():
-    return ''
+    
+    if request.method == 'GET':
+        messages = []
+        for message in Message.query.order_by(Message.created_at).all():
+            message_dict = message.to_dict()
+            messages.append(message_dict)
 
-@app.route('/messages/<int:id>')
+        response = make_response(
+            messages,
+            200
+        )
+        return response
+
+    elif request.method == 'POST':
+        new_message = Message(
+            body = request.get_json()['body'],
+            username = request.get_json()['username'],
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+
+        response = make_response(
+            message_dict,
+            201
+        )
+
+        return response
+
+
+@app.route('/messages/<int:id>', methods = ['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    
+    message = Message.query.filter(Message.id == id).first()
+
+    if request.method == 'GET':
+
+        message_dict = message.to_dict()
+
+        response = make_response(
+            message_dict,
+            200
+        )
+    
+        return response
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            'delete_successful' : True,
+            'message' : 'Message deleted.'
+        }
+
+        response = make_response(
+            response_body,
+            200
+        )
+
+        return response
+
+    elif request.method == 'PATCH':
+
+        patch_message = request.get_json()
+
+        for attr in patch_message:
+            setattr(message, attr, patch_message[attr])
+
+        db.session.add(message)
+        db.session.commit()
+
+        message_dict = message.to_dict()
+
+        response = make_response(
+            message_dict,
+            200
+        )
+
+        return response
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=4000)
